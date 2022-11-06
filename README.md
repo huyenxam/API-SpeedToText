10a748d3cdfa3b4e24762ee05ab78027bdd0dfef
huyenxam <huyenxam250400@gmail.com>
2022-11-06 21:02:00 +0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# API-SpeedToText
+API-SpeedToText
+## Cấu trúc thư mục
+---API_S2T
+|	|--- static
+|	|	|---css
+|	|	|	|---main.css
+|	|	|---js
+|	|	|	|---scripts.js
+|	|	|---uploads
+|	|	|---video
+|	|	|	|---video.mp4
+|	|---templates
+|	|	|---index.html
+|	|---api.zip
+|	|---flask_app.py
+|	|---model.py
+
+## Câu lệnh thực thi
+
+```bash
+python .\flask_app.py
+```
+
+## Demo
+<video controls>
+  <source src=./static/video/video.mp4" type="video/mp4">
+</video>
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from preprocessing_audio import preprocessing
+from time import sleep
 
 app = Flask(__name__, static_url_path="/static", static_folder='./static')
 CORS(app)
@@ -28,9 +29,10 @@ def predic_upload():
         audios = preprocessing("./static/upload/", audio_path)
         # print(audio_paths)
 
+        
         # model dự đoán
-        predict = "Nguyen Thi Ngoc Huyen"
-
+        predict = "dạ mobifone xin nghe em là minh anh có thể giúp được gì cho anh chị ạ chị kiểm tra giùm em cái số điện thoại này gọi lên đó em mới cài ba g d mười á mà không hoạt động nó không chạy được luôn á chị dạ vâng rất xin lỗi về sự bất tiện này cho em xin anh tên gì để em tiện xưng hô được không ạ dạ em tên minh dạ em chào anh minh số anh minh đây em kiểm tra là đúng là có đăng ký gói cước d mười cho mở thiết bị di động máy hiện ba g bốn g hay kiểu gì vậy anh minh dạ chữ h chị dạ vâng mình sẽ hỗ trợ đóng nối mở nối lại đường truyền truy cập mạng cho sim của anh minh lại kết thúc cuộc gọi này anh thông cảm tắt nguồn máy hai phút sau anh mở nguồn nên bật lại dùng lại thử xem được chưa nha được rồi ok em cám ơn dạ mình cần hỗ trợ thông tin"
+        sleep(1.5)
     return render_template('index.html', predict=predict, audio_path=audio_path)
 
 
